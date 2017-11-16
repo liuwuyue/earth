9140440f18205eaba04fb64f04567f9d19c45597
敏岳 <wuyue.lwy@alibaba-inc.com>
2017-11-16 11:00:32 +0800

    打包处理

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,10 +1,9 @@
 var webpack = require('atool-build/lib/webpack');
 
 module.exports = function(webpackConfig) {
-  webpackConfig.output.publicPath = '/assets/earth/dist/';
+  webpackConfig.output.publicPath = '/dist/';
   // 添加一个plugin
   webpackConfig.babel.plugins.push('transform-runtime');
-  // 返回 webpack 配置对象
   return webpackConfig;
 };
 
