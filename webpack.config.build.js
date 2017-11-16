06acfac11697da402f0f3dda6132fdb1db089b63
敏岳 <wuyue.lwy@alibaba-inc.com>
2017-11-16 15:38:48 +0800

    添加版本号便于发布

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,7 +1,8 @@
 var webpack = require('atool-build/lib/webpack');
-
+let packageJson = require('./package.json');
+let version = packageJson.version; 
 module.exports = function(webpackConfig) {
-  webpackConfig.output.publicPath = '/dist/';
+  webpackConfig.output.publicPath = '/dist/' + version + '/';
   // 添加一个plugin
   webpackConfig.babel.plugins.push('transform-runtime');
   return webpackConfig;
